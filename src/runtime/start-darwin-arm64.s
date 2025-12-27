e99aaa8ce5efac0a6a09b731f758dcb48b33f42d
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-12-27 01:02:53 +0100

    upgrade: setup arm64 runtime

---
+++ src/runtime/start-darwin-arm64.s
@@ -0,0 +1,43 @@
+# honey arm64 runtime
+
+.text
+
+# main entry point
+.global _start
+.align 2
+_start:
+  # stack layout at _start entry:
+  # [sp + 0]  = argc (argument count)
+  # [sp + 8]  = argv[0] (pointer to program name)
+  # [sp + 16] = argv[1] (pointer to first argument)
+  # [sp + 24] = argv[2] (pointer to second argument)
+  # ...
+  # [sp + ?]  = NULL (terminator)
+
+  # function prologue start
+  # function prologue end
+
+  bl _main  ; call user defined main function
+
+  # function epilogue start
+  # function epilogue end
+
+  # exit with the return code from main
+  mov x16, #1   ; syscall number for exit
+  svc #0        ; trigger syscall
+
+# test runner entry point
+.global _test_start
+.align 2
+_test_start:
+  # function prologue start
+  # function prologue end
+
+  bl _test_runner  ; call test runner
+
+  # function epilogue start
+  # function epilogue end
+
+  # exit with the return code from test runner
+  mov x16, #1   ; syscall number for exit
+  svc #0        ; trigger syscall
